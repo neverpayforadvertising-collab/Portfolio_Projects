cf8dafd49e32b368e0a49a4d9bf6372227b75b5e
fullstackengineer <neverpayforadvertising@gmail.com>
2026-07-20 09:48:04 -0500

    Update jest.config.js

--- React_Node.js_Catalog_Management/backend/jest.config.js
+++ React_Node.js_Catalog_Management/backend/jest.config.js
@@ -3,5 +3,10 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  // CHANGED: map NodeNext-style "./foo.js" imports in TS sources back to "./foo"
+  // so ts-jest can resolve them when running tests.
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
   verbose: true
 };
